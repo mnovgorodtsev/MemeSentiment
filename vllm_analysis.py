8f63beed371b909d1d47e905cb9f912de55864d1
katarzynamichalska <kate.michalska7@gmail.com>
2025-11-27 20:37:57 +0000

    poc vllm

---
+++ vllm_analysis.py
@@ -0,0 +1,42 @@
+import ollama
+import pandas as pd
+
+
+df = pd.read_csv("./data/memotion_dataset_7k/labels.csv")
+
+
+df["humour"] = df["humour"].replace(
+    ["hilarious", "very_funny", "funny"],
+    "funny"
+)
+
+df["sarcasm"] = df["sarcasm"].replace(
+    ["twisted_meaning", "very_twisted"],
+    "twisted"
+)
+
+df["offensive"] = df["offensive"].replace(
+    ["very_offensive", "slight", "hateful_offensive"],
+    "offensive"
+)
+
+
+print(df.loc[45])
+
+
+prompts = ["Classify if this mem is funny or not funny. Return only class: funny or not funny",
+           "Classify if this mem is general, twisted or not sarcastic. Return only class: general, twisted or not sarcastic",
+           "Classify if this mem is offensive or not offensive. Return only class: offensive or not offensive",
+           "Classify if this mem is motivational or not motivational. Return only class: motivational or "
+           "not motivational"
+           ]
+
+for prompt in prompts:
+    response = ollama.chat(model='qwen3-vl:2b',
+        messages=[{
+            'role': 'user',
+            'content': prompt,
+            'images': ["./data/memotion_dataset_7k/images/image_46.jpg"]
+        }],
+        )
+    print(response['message']['content'])
